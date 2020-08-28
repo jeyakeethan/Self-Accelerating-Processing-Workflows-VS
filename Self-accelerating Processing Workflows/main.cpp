4c1d52a3e1b1082d84972dc6e24e53f6298b4a6d
JK <jeyakeethan@gmail.com>
2020-08-28 13:35:09 +0530

    Auto logging added and Odd input generation code added

--- Self-accelerating Processing Workflows/main.cpp
+++ Self-accelerating Processing Workflows/main.cpp
@@ -14,11 +14,13 @@
 #include <ArrayAdditionModel.h>
 #include <DotMultiplicationModel.h>
 #include <random>
+#include <string>
 
 using namespace std;
 int main()
 {
     LARGE_INTEGER start, stop, clockFreq;
+    ofstream outfile;
     QueryPerformanceFrequency(&clockFreq);
     double delay;
     int elapsedTime;
@@ -27,15 +29,16 @@ int main()
     int** arraySet1 = new int* [EXPERIMENT_COUNT];
     int** arraySet2 = new int* [EXPERIMENT_COUNT];
     int* arrayLength = new int[EXPERIMENT_COUNT];
-    int x;
+    int x, k, length, fileNum;
 
     /*---Random Seed Value---*/
     srand(5);
 
     if (GENERATE_ALIGNED_INPUT) {
         /*********Generate Aligned Input Stream*********/
-        int k, length, widthCount = 0, width = rand() % MAX_WIDTH_ALIGNED + 1;
+        int widthCount = 0, width = rand() % MAX_WIDTH_ALIGNED + 1;
         bool iSmall = true;
+        int *temp1, *temp2;
         for (x = 0; x < EXPERIMENT_COUNT; x++) {
             if (++widthCount > width) {
                 //cout << "width: " << width << endl << endl;
@@ -47,8 +50,8 @@ int main()
             else length = rand() % (ARRAY_MAX_LENGTH - SMALL_ARRAY_MAX_LENGTH) + SMALL_ARRAY_MAX_LENGTH + 1;
             //cout << "length: " << length << endl;
             arrayLength[x] = length;
-            int* temp1 = new int[length];
-            int* temp2 = new int[length];
+            temp1 = new int[length];
+            temp2 = new int[length];
             arraySet1[x] = temp1;
             arraySet2[x] = temp2;
             for (k = 0; k < length; k++) {
@@ -59,13 +62,30 @@ int main()
     }
     else {
         /*********Generate Odd Input Stream*********/
+        int *temp1, *temp2;
+        for (x = 0; x < EXPERIMENT_COUNT; x++) {
+            length = rand() % ARRAY_MAX_LENGTH + 1;
+            //cout << "length: " << length << endl;
+            arrayLength[x] = length;
+            temp1 = new int[length];
+            temp2 = new int[length];
+            arraySet1[x] = temp1;
+            arraySet2[x] = temp2;
+            for (k = 0; k < length; k++) {
+                temp1[k] = rand() % RANGE_OF_INT_VALUES;
+                temp2[k] = rand() % RANGE_OF_INT_VALUES;
+            }
+        }
     }
 
     /************Write Input Nature into File************/
-    ofstream outfile;
-    outfile.open("Input Nature.csv");
+    string inputNatureFile = "Input Nature.csv"; fileNum = 0;
+    while(FILE* file = fopen(inputNatureFile.c_str(), "r")) {
+        fclose(file);
+        inputNatureFile = "Input Nature_" + to_string(++fileNum) + ".csv";
+    }
+    outfile.open(inputNatureFile);
 
-    int y;
     for (x = 0; x < EXPERIMENT_COUNT; x++) {
         int len = arrayLength[x];
         outfile << len << endl;
@@ -75,6 +95,12 @@ int main()
     }
     outfile.close();
 
+    string timeLogFile = "Time.log"; fileNum = 0;
+    while (FILE* file = fopen(timeLogFile.c_str(), "r")) {
+        fclose(file);
+        timeLogFile = "Time_" + to_string(++fileNum) + ".log";
+    }
+    outfile.open(timeLogFile);
     /**********Self flow experiment - ArrayAdditionModel**********/
     QueryPerformanceCounter(&start);
     for (x = 0; x < EXPERIMENT_COUNT; x++) {
@@ -89,6 +115,7 @@ int main()
     delay = (double)(stop.QuadPart - start.QuadPart) / (double)clockFreq.QuadPart;
     elapsedTime = int(delay * 1000);
     cout << "Self Flow Time: " << elapsedTime << " ms" << endl << endl;
+    outfile << "Self Flow Time: " << elapsedTime << " ms" << endl << endl;
     
     QueryPerformanceCounter(&start);
     for (x = 0; x < EXPERIMENT_COUNT; x++) {
@@ -103,6 +130,7 @@ int main()
     delay = (double)(stop.QuadPart - start.QuadPart) / (double)clockFreq.QuadPart;
     elapsedTime = int(delay * 1000);
     cout << "CPU Only Time: " << elapsedTime << " ms" << endl << endl;
+    outfile << "CPU Only Time: " << elapsedTime << " ms" << endl << endl;
     
     
     QueryPerformanceCounter(&start);
@@ -118,6 +146,8 @@ int main()
     delay = (double)(stop.QuadPart - start.QuadPart) / (double)clockFreq.QuadPart;
     elapsedTime = int(delay * 1000);
     cout << "GPU Only Time: " << elapsedTime << " ms" << endl << endl;
+    outfile << "GPU Only Time: " << elapsedTime << " ms" << endl << endl;
+    outfile.close();
 
     /*
 
