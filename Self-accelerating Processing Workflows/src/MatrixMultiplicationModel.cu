69fd3428b98c88c6e946d44b53ad8bf923765aa6
JK <jeyakeethan@gmail.com>
2020-09-15 23:12:40 +0530

    mat multiplication completed

--- Self-accelerating Processing Workflows/src/MatrixMultiplicationModel.cu
+++ Self-accelerating Processing Workflows/src/MatrixMultiplicationModel.cu
@@ -14,59 +14,59 @@
 using namespace std;
 
 template <class T>
-MatrixMultiplicationModel<T>::MatrixMultiplicationModel(int CPUCores):ComputationalModel(CPUCores){
-    //super(CPUCores);
+MatrixMultiplicationModel<T>::MatrixMultiplicationModel(int CPUCores) :ComputationalModel(CPUCores) {
+	//super(CPUCores);
 }
 
 template <class T>
 MatrixMultiplicationModel<T>::~MatrixMultiplicationModel() {}
 
 template <class T>
-void MatrixMultiplicationModel<T>::CPUImplementation(){
-    // implement using multi threads
-    #pragma omp parallel num_threads(CPUCores)
-    {
-        #pragma omp for
-        for (int i = 0; i < localMD->x; i++) {
-            for (int j = 0; j < localMD->y; j++) {
-                localC[localMD->z * i + j] = 0;
-                for (int k = 0; k < localMD->y; k++) {
-                    localC[localMD->z * i + j] += localA[localMD->y * i + k] * localB[j + localMD->z * k];
-                }
-            }
-        }
-        #pragma omp barrier
-    }
+void MatrixMultiplicationModel<T>::CPUImplementation() {
+	// implement using multi threads
+#pragma omp parallel num_threads(CPUCores)
+	{
+#pragma omp for
+		for (int i = 0; i < localMD->x; i++) {
+			for (int j = 0; j < localMD->y; j++) {
+				localC[localMD->z * i + j] = 0;
+				for (int k = 0; k < localMD->y; k++) {
+					localC[localMD->z * i + j] += localA[localMD->y * i + k] * localB[j + localMD->z * k];
+				}
+			}
+		}
+#pragma omp barrier
+	}
 }
 
 template <class T>
-void MatrixMultiplicationModel<T>::GPUImplementation(){
-    //Device array
-    numericalType1 *dev_a , *dev_b, *dev_c;
+void MatrixMultiplicationModel<T>::GPUImplementation() {
+	//Device array
+	numericalType1* dev_a, * dev_b, * dev_c;
 
-    int l1 = localMD->x * localMD->y * sizeof(numericalType1);
-    int l2 = localMD->y * localMD->z * sizeof(numericalType1);
-    int l3 = localMD->x * localMD->z * sizeof(numericalType1);
+	int l1 = localMD->x * localMD->y * sizeof(numericalType1);
+	int l2 = localMD->y * localMD->z * sizeof(numericalType1);
+	int l3 = localMD->x * localMD->z * sizeof(numericalType1);
 
-    //Allocate the memory on the GPU
-    cudaMalloc((void **)&dev_a , l1);
-    cudaMalloc((void **)&dev_b , l2);
-    cudaMalloc((void **)&dev_c , l3);
+	//Allocate the memory on the GPU
+	cudaMalloc((void**)&dev_a, l1);
+	cudaMalloc((void**)&dev_b, l2);
+	cudaMalloc((void**)&dev_c, l3);
 
-    //Copy Host array to Device array
-    cudaMemcpy (dev_a , localA , l1, cudaMemcpyHostToDevice);
-    cudaMemcpy (dev_b , localB , l2, cudaMemcpyHostToDevice);
-    // Execute the kernel
-    // define grid and thread block sizes
+	//Copy Host array to Device array
+	cudaMemcpy(dev_a, localA, l1, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_b, localB, l2, cudaMemcpyHostToDevice);
+	// Execute the kernel
+	// define grid and thread block sizes
 
-    dim3 dimGrid((l3/1024/32+1), 1024), dimBlock(32);
-    matrix_multiplication <<<dimGrid, dimBlock>>> (dev_a, dev_b, dev_c, localMD->x);
-    //Copy back to Host array from Device array
-    cudaMemcpy(localC , dev_c , l3, cudaMemcpyDeviceToHost);
-    //Free the Device array memory
-    cudaFree (dev_a);
-    cudaFree (dev_b);
-    cudaFree (dev_c);
+	dim3 dimGrid((l3 / 1024 / 32 + 1), 1024), dimBlock(32);
+	matrix_multiplication << <dimGrid, dimBlock >> > (dev_a, dev_b, dev_c, localMD->x);
+	//Copy back to Host array from Device array
+	cudaMemcpy(localC, dev_c, l3, cudaMemcpyDeviceToHost);
+	//Free the Device array memory
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_c);
 }
 
 #endif // _MATRIXMULTIPLICATIONMODEL_CPP_
