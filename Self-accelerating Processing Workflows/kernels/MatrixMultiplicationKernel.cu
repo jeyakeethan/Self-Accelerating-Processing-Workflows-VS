f65e0f41ed626764d38568180de3331d9818db25
JK <jeyakeethan@gmail.com>
2020-10-26 11:29:52 +0530

    matmul kernel bug fixed

--- Self-accelerating Processing Workflows/kernels/MatrixMultiplicationKernel.cu
+++ Self-accelerating Processing Workflows/kernels/MatrixMultiplicationKernel.cu
@@ -3,10 +3,10 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ void matrix_multiplication(float* C, float* A, float* B, const int width) {
+__global__ void matrix_multiplication(numericalType1* A, numericalType1* B, numericalType1* C, const int width) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
-    float sum = 0.0;
+    numericalType1 sum = 0;
 
     for (int k = 0; k < width; k++) {
         sum += A[y * width + k] * B[k * width + x];
