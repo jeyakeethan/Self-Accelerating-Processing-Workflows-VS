7162699bee15f2644144454621d9ba09c41ad14c
JK <jeyakeethan@gmail.com>
2020-10-28 13:14:11 +0530

    kernel bug fixed

--- Self-accelerating Processing Workflows/headers/kernels.h
+++ Self-accelerating Processing Workflows/headers/kernels.h
@@ -1,6 +1,6 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ void matrix_multiplication(float* C, float* A, float* B, const int width);
+__global__ void matrix_multiplication(float* C, float* A, float* B, const int widthA, const int widthB);
 __global__ void Vector_Addition(const int* dev_a, const int* dev_b, int* dev_c);
 __global__ void dot_product(int* a, int* b, int* res);
--- Self-accelerating Processing Workflows/kernels/MatrixMultiplicationKernel.cu
+++ Self-accelerating Processing Workflows/kernels/MatrixMultiplicationKernel.cu
@@ -3,14 +3,14 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-__global__ void matrix_multiplication(numericalType1* A, numericalType1* B, numericalType1* C, const int width) {
+__global__ void matrix_multiplication(numericalType1* A, numericalType1* B, numericalType1* C, const int widthA, const int widthB) {
     int x = blockIdx.x * blockDim.x + threadIdx.x;
     int y = blockIdx.y * blockDim.y + threadIdx.y;
     numericalType1 sum = 0;
 
-    for (int k = 0; k < width; k++) {
-        sum += A[y * width + k] * B[k * width + x];
+    for (int k = 0; k < widthA; k++) {
+        sum += A[y * widthA + k] * B[k * widthB + x];
     }
 
-    C[y * width + x] = sum;
+    C[y * widthB + x] = sum;
 }
