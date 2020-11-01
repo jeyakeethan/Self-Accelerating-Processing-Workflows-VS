b18f0649ddce234a2c85e1d41688fbb2729022f6
JK <jeyakeethan@gmail.com>
2020-11-02 01:22:59 +0530

    type def to int

--- Self-accelerating Processing Workflows/headers/kernels.h
+++ Self-accelerating Processing Workflows/headers/kernels.h
@@ -1,5 +1,6 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <Constants.h>
 
 __global__ void matrix_multiplication(numericalType1* C, numericalType1* A, numericalType1* B, const int widthA, const int widthB);
 __global__ void Vector_Addition(const int* dev_a, const int* dev_b, int* dev_c);
