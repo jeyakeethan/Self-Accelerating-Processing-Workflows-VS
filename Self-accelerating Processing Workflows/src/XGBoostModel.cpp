1848ca58fab17c04682d8306b00bbfa5c496b214
JK <jeyakeethan@gmail.com>
2020-11-07 00:12:30 +0530

    ML prediction fuctions added to ComModel

---
+++ Self-accelerating Processing Workflows/src/XGBoostModel.cpp
@@ -0,0 +1,17 @@
+#include <Windows.h>
+#include <iostream>
+#include <thread>
+#include <sstream>
+#include <Constants.h>
+using namespace std;
+
+class MatrixMulXGBoostModel {
+	public:
+		MatrixMulXGBoostModel() {}
+		static int predict(int* params) {
+			// TO DO decison of the model here
+			return 0;
+		}
+	private:
+
+};
