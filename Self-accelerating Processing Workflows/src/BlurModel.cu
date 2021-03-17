0c9e6523258b9a5764d0cb26dc3af76224859acf
Keethan <Keethan@gmail.com>
2021-03-17 21:14:26 +0530

    CPU code BlurModel corrected and works

--- Self-accelerating Processing Workflows/src/BlurModel.cu
+++ Self-accelerating Processing Workflows/src/BlurModel.cu
@@ -28,29 +28,39 @@ void BlurModel<T>::CPUImplementation() {
 
     int fsize = 5; // Filter size
 
-    float output_red = 0;
-    float output_green = 0;
-    float output_blue = 0;
-    int hits = 0;
-    for (int y = 0; y < height; y++) {
-        int offset = y * width;
-        for (int x = offset; x < offset+width; x++) {
-            for (int ox = -fsize; ox < fsize + 1; ++ox) {
-                for (int oy = -fsize; oy < fsize + 1; ++oy) {
-                    if ((x + ox) > -1 && (x + ox) < width && (y + oy) > -1 && (y + oy) < height) {
-                        const int currentoffset = (offset + ox + oy * width) * 3;
-                        output_red += input_image[currentoffset];
-                        output_green += input_image[currentoffset + 1];
-                        output_blue += input_image[currentoffset + 2];
-                        hits++;
+    int size = width * height;
+    //implement using multi threads
+#pragma omp parallel num_threads(CPUCores)
+    {
+#pragma omp for
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int offset = y * width + x;
+                if (offset < size) {
+                    float output_red = 0;
+                    float output_green = 0;
+                    float output_blue = 0;
+                    int hits = 0;
+                    for (int ox = -fsize; ox < fsize + 1; ++ox) {
+                        for (int oy = -fsize; oy < fsize + 1; ++oy) {
+                            if ((x + ox) > -1 && (x + ox) < width && (y + oy) > -1 && (y + oy) < height) {
+                                const int currentoffset = (offset + ox + oy * width) * 3;
+                                output_red += input_image[currentoffset];
+                                output_green += input_image[currentoffset + 1];
+                                output_blue += input_image[currentoffset + 2];
+                                hits++;
+                            }
+                        }
                     }
+                    output_image[offset * 3] = output_red / hits;
+                    output_image[offset * 3 + 1] = output_green / hits;
+                    output_image[offset * 3 + 2] = output_blue / hits;
                 }
             }
-            output_image[offset * 3] = output_red / hits;
-            output_image[offset * 3 + 1] = output_green / hits;
-            output_image[offset * 3 + 2] = output_blue / hits;
         }
+#pragma omp barrier
     }
+
 }
 
 template <class T>
